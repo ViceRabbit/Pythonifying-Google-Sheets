85e26f1fc1833791a29cd0ee403d40122519fe81
ViceRabbit <vicerabbit16789@gmail.com>
2023-11-14 01:15:08 +0000

    oop & helpful funcs

--- main.py
+++ main.py
@@ -6,15 +6,35 @@ eyepod_spreadsheet = serviceAccount.open("Personality questions for SCP-131 test
 
 formatWorksheet = eyepod_spreadsheet.worksheet("Format")
 
-#print(f"amount of rows: {worksheet.row_count}")
-#print (f"amount of columns: {worksheet.col_count}")
-#print(worksheet.acell('a4').value)
-#print(worksheet.get('a2:d17'))
+def clearrow(sheet, row): # a quick function to clear all checkboxes in altering choices
+    sheet.update_cell(row, 2, False)
+    sheet.update_cell(row, 3, False)
+    sheet.update_cell(row, 4, False)
 
-#worksheet.update('b2:d3', [[True, True, True], [True, False, True]])
+class initialize131Worksheet():
+    def __init__(self, session, colorOfEyepod):
+        self.session = session
+        self.colorOfEyepod = colorOfEyepod
+    def createsheet(self):
+        self.thesheet = formatWorksheet.duplicate(len(eyepod_spreadsheet.worksheets()), None, f"Session {str(self.session)} - {self.colorOfEyepod}")
 
-#print(worksheet.get_all_values())
+class change131worksheet():
+    def __init__(self, thesheet):
+        self.thesheet = thesheet
+    def changecheckbox(self, questionInt, boolofchoice="idk", triggerbox=True):
+        self.rowOfQuestion = questionInt + 1
+        clearrow(self.thesheet, self.rowOfQuestion)
+        self.choiceOfCol = 4 # idk column; already set
+        if boolofchoice == "Yes":
+            self.choiceOfCol = 2 # yes column
+        if boolofchoice == "No":
+            self.choiceOfCol = 3 # no column
+        self.triggerbox = triggerbox
+        
+        self.thesheet.update_cell(self.rowOfQuestion, self.choiceOfCol, self.triggerbox)
+        
 
-newthing = formatWorksheet.duplicate(len(eyepod_spreadsheet.worksheets()), None, "goofy gooberonis!") #duplicates from format; indexes at end
-
+ss = change131worksheet(eyepod_spreadsheet.worksheet("Session 3 - Blue + Orange"))
+ss.changecheckbox(1, "Yes")
+        
 print("tada!!!")
