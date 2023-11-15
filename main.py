8dde2772acfb95b7256f44367f6d33941ebb694a
ViceRabbit <vicerabbit16789@gmail.com>
2023-11-15 00:05:28 +0000

    funny changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import gspread 
+import time
 
 serviceAccount = gspread.service_account("/workspace/Pythonifying-Google-Sheets/gspread/service_account.json")
 
@@ -6,10 +7,14 @@ eyepod_spreadsheet = serviceAccount.open("Personality questions for SCP-131 test
 
 formatWorksheet = eyepod_spreadsheet.worksheet("Format")
 
+
+
 def clearrow(sheet, row): # a quick function to clear all checkboxes in altering choices
     sheet.update_cell(row, 2, False)
     sheet.update_cell(row, 3, False)
     sheet.update_cell(row, 4, False)
+
+
 
 class initialize131Worksheet():
     def __init__(self, session, colorOfEyepod):
@@ -33,25 +38,42 @@ class change131worksheet():
         
         self.thesheet.update_cell(self.rowOfQuestion, self.choiceOfCol, self.triggerbox)
         
+def apiexaust(obj, questionnumber, booleo): # a quick function for API fallbacks for google
+    print("Experiencing API exaustion! - Waiting for 25 seconds")
+    time.sleep(25)
+    obj.changecheckbox(questionnumber, booleo)
+    print("We're back!")
+
+def changeRResponses(boolinstances, sessionnumber, instcolorlists, questionnum, boolchoice): # neatly change large reqs
+    for instance in boolinstances:
+        if instance == "None":
+            break
+        theobj = change131worksheet(eyepod_spreadsheet.worksheet(f"Session {sessionnumber} - {instcolorlists[int(instance)]}"))
+        try:
+            theobj.changecheckbox(questionnum, boolchoice)
+        except:
+            apiexaust(theobj, questionnum, boolchoice)
 
 # create multiple sheets for every 131 instance 
 sessionNumber = input("What session number is this? - ")
 colorOfInstances = input("Provide the base + iris color of the instances seperated in commas - ").split(", ")
-amountOfInstances = len(colorOfInstances)
 
 for entity in colorOfInstances:
     initialize131Worksheet(sessionNumber, entity).createsheet()
     print(f"Successfully created a worksheet for {entity} under Session {sessionNumber}.")
 
-reference = list(enumerate(colorOfInstances))
-print(f"For reference, please utilize integers to refer to specific 131 instances as given by the enumerated sheet. \n {reference}")
+print(f"For reference, please utilize integers to refer to specific 131 instances as given by the enumerated sheet. \n {list(enumerate(colorOfInstances))}")
 
 # 16 questions
-for x in range(16):
-    print(f"Question {x}")
-    
+for x in range(1, 17):
+    print(f"Question {x} - {formatWorksheet.cell(x+1, 1)} \n Ensure you respond with the enumerated integers & seperated in commas; if no instances, reply with None.")
+    yesinstances = input("Provide the instances that said yes - ").split(", ")
+    noinstances = input("Provide the instances that said no - ").split(", ")
+    idkinstances = input("Provide the instances that said idk - ").split(", ")
 
-
+    changeRResponses(yesinstances, sessionNumber, colorOfInstances, x, "Yes") # for yes responses
+    changeRResponses(noinstances, sessionNumber, colorOfInstances, x, "No") # for no responses
+    changeRResponses(idkinstances, sessionNumber, colorOfInstances, x, "idk") # for idk responses
 
 
 #ss = change131worksheet(eyepod_spreadsheet.worksheet("Session 3 - Blue + Orange"))
